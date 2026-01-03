ffbd7d08bfe911922c9a8a31604303d0d85a0ead
anfigeno <dolorcriticodevastador@proton.me>
2026-01-03 15:51:38 -0500

    Primer compromiso

---
+++ flake.nix
@@ -0,0 +1,8 @@
+{
+  description = "Flake de Mestizo Nix";
+
+  outputs = { self }: {
+    paleta = import ./src/paleta.nix;
+    paletaExtendida = import ./src/paletaExtendida.nix;
+  };
+}
---
+++ src/paleta.nix
@@ -0,0 +1,27 @@
+{
+  rojo = "#ff5263";
+  amarillo = "#ffe780";
+  verde = "#8eeda1";
+  azul = "#8B93FF";
+  magenta = "#F27AFF";
+  cian = "#6ae4fc";
+  rosa = "#FF808C";
+  naranja = "#fc9b74";
+  lima = "#bfed8e";
+  fucsia = "#e4adff";
+  celeste = "#8bb6ff";
+  tope3 = "#f0f0fe";
+  tope2 = "#e0e1fd";
+  tope1 = "#d1d1fc";
+  tope = "#C1C2FB";
+  tope_1 = "#9596ca";
+  tope_2 = "#686998";
+  tope_3 = "#3c3d67";
+  base3 = "#2b3246";
+  base2 = "#272c3d";
+  base1 = "#222533";
+  base = "#1C1C26";
+  base_1 = "#191921";
+  base_2 = "#131418";
+  base_3 = "#0c0c0e";
+}
